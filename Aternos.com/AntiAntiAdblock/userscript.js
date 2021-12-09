3dc9c400d3b4d668fae91cc3d546007f917ce14a
Haka <76921756+Hakorr@users.noreply.github.com>
2021-12-09 18:55:51 +0200

    V2.1 - Fixed the script

--- Aternos.com/AntiAntiAdblock/userscript.js
+++ Aternos.com/AntiAntiAdblock/userscript.js
@@ -3,15 +3,41 @@
 // @namespace   HKR
 // @match       https://aternos.org/*
 // @grant       none
-// @version     2.0
+// @version     2.1
 // @author      HKR
 // @description Removes all the adblock reminders without a hussle.
 // @require     https://greasyfork.org/scripts/21927-arrive-js/code/arrivejs.js
 // @run-at      document-start
 // ==/UserScript==
 
+//Found the advertisement element on document -> Hide all the nondeleted advertisement elements
+document.arrive('.ad', function () { 
+    Array.from(document.querySelectorAll(".ad")).forEach(ad => ad.setAttribute("style","display: none;")); 
+});
+
+//Found the start button on document -> Patch the start button (To make it functional)
+document.arrive('#start', function () { 
+    document.querySelector("#start").onclick = function() {
+        start();
+    }
+});
+
+//Found the navigation open/close button on document -> Patch the navigation open/close button (To make it functional)
+document.arrive('.navigation-toggle', function () { 
+    document.querySelector(".navigation-toggle").onclick = function(){
+        let nav = document.querySelector(".navigation"); 
+        nav.setAttribute("class", nav.getAttribute("class") == "navigation" ? "navigation toggled" : "navigation");
+    }
+});
+
+//Found the sidebar element on document -> Hide sidebar (Only used for advertisements)
+document.arrive('.sidebar', function () { 
+    document.querySelector(".sidebar").setAttribute("style","display: none;"); 
+});
+
 /* onbeforescriptexecute - https://github.com/Ray-Adams/beforeScriptExecute-Polyfill 
 Makes catching all web requests and blocking them if necessary possible. */
+
 (() => {
     'use strict';
 
@@ -50,24 +76,15 @@ Makes catching all web requests and blocking them if necessary possible. */
 
 //A new web request initiated
 document.onbeforescriptexecute = (e) => {
-  //If it requests a base64 encoded js file
-  if (e.target.src.includes('data:text/javascript;base64,')) {
-    //Block it
-    e.preventDefault();
-  }
+    //If it requests a base64 encoded js file
+    if (e.target.src.includes('data:text/javascript;base64,')) {
+        //Block it
+        e.preventDefault();
+    }
 }
 
-//Found advertisement element on document
-document.arrive('.ad', function () {
-    //Remove all the nondeleted advertisement elements
-    Array.from(document.querySelectorAll(".ad")).forEach(ad => ad.setAttribute("style","display: none;"));
-});
-
-//Found start button on document
-document.arrive('#start', function () {
-    //Patch the start button to make it functional
-    document.querySelector("#start").setAttribute("onclick","start()");
-});
+//Find and "hide" a restrictive top element
+function removeLayer() { Array.from(document.querySelectorAll("[style]")).forEach(elim => { if(elim.getAttribute("style").includes("top: 0;")) elim.style += "top: -1px"; }); }
 
 //Document loaded
 window.addEventListener('load', function () {
@@ -76,11 +93,3 @@ window.addEventListener('load', function () {
         removeLayer();
     });
 });
-
-//Find and remove a restrictive top element
-function removeLayer() {
-    Array.from(document.querySelectorAll("*[style]")).forEach(elim => {
-        if(elim.getAttribute("style").includes("top: 0px;"))
-            elim.style += "top: -1px";
-    });
-}
