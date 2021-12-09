1c7eb64834f6b190476d4127ace8a9158a9698a2
Haka <76921756+Hakorr@users.noreply.github.com>
2021-12-09 23:37:55 +0200

    V1.1 - Script working again

--- Pixlr.com/RemoveBlackSidebar/userscript.js
+++ Pixlr.com/RemoveBlackSidebar/userscript.js
@@ -3,17 +3,20 @@
 // @namespace   HKR
 // @match       https://pixlr.com/*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      HKR
 // @description Removes the annoying black sidebar that Adblockers didn't remove.
 // @require     https://greasyfork.org/scripts/21927-arrive-js/code/arrivejs.js
 // @supportURL  https://github.com/Hakorr/Userscripts/issues
-// @grant GM_addStyle
+// @grant       GM_addStyle
 // ==/UserScript==
 
 GM_addStyle("#workspace { right: 0px !important; }");
 
-const elementToWatch = '#slot';
-document.arrive(elementToWatch, function () {
+document.arrive('#slot', function () {
 	document.getElementById("slot").remove();
+});
+
+document.arrive('div[id="right-space"]', function () {
+	document.getElementById("right-space").remove();
 });
