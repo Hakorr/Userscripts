02741c075b360f5349df04388aea699f1a0c7614
Haka <76921756+Hakorr@users.noreply.github.com>
2021-12-12 04:31:07 +0200

    V3.0 - Script works, once again...

--- Aternos.com/AntiAntiAdblock/userscript.js
+++ Aternos.com/AntiAntiAdblock/userscript.js
@@ -3,56 +3,87 @@
 // @namespace   HKR
 // @match       https://aternos.org/*
 // @grant       none
-// @version     2.7
+// @version     3.0
 // @author      HKR
 // @description Removes all the adblock reminders without a hussle.
 // @supportURL  https://github.com/Hakorr/Userscripts/issues
 // @downloadURL https://raw.githubusercontent.com/Hakorr/Userscripts/main/Aternos.com/AntiAntiAdblock/userscript.js
-// @require     https://greasyfork.org/scripts/21927-arrive-js/code/arrivejs.js
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.js
 // @run-at      document-start
 // ==/UserScript==
 
-/* Quite clever, devs. */
+/* NOTE THAT THE SCRIPT MIGHT NOT WORK AS ATERNOS' DEVELOPERS DO NOT SLEEP AND MESS WITH THIS SCRIPT ALMOST EVERY HOUR
+- It's almost pointless to make public userscripts as the developers will just change a minor detail on their HTML or JS, which totally messes something up. 
+- I have my own private script that works, and will only share it with trusted people. 
 
-//Found the advertisement element on document -> Hide all the nondeleted advertisement elements
-document.arrive('.ad', function () { 
-    Array.from(document.querySelectorAll(".ad")).forEach(ad => ad.setAttribute("style","display: none;")); 
+That's all, if it doesn't work, please don't cry and make your own one, keep it private. */
+
+/* Note to the developers, touch some grass. */
+
+$(document).ready(function () {
+    Array.from($(".ad")).forEach(ad => ad.setAttribute("style","display: none;")); 
+  
+    $(".body, .header").each(function () {
+        this.style.setProperty("display", "");
+        this.style.setProperty("height", "");
+    });
+
+    $("#start").each(function () {
+        this._ready = true;
+    });
+
+    $("#userdropdown-toggle").click(function (e) {
+        if ($(window).width() <= 1e3) {
+        e.preventDefault();
+        $(".userdropdown").slideToggle(100);
+        }
+    });
+
+    $(".logout").click(function () {
+        aget("/panel/ajax/account/logout.php", function () {
+        location.href = "/go/";
+        });
+    });
+    $(".navigation-toggle").click(function () {
+        var cookieValue = 0;
+        if ($(".navigation").hasClass("toggled")) {
+        $(".navigation").removeClass("toggled");
+        } else {
+        $(".navigation").addClass("toggled");
+        cookieValue = 1;
+        }
+        document.cookie = COOKIE_PREFIX + "_NAVIGATION_TOGGLED=" + cookieValue + ";path=/;max-age=31536000";
+    });
+    $(".friend-access-count-dropdown").click(function () {
+        var dropdown = $(".friend-access-dropdown");
+        if (dropdown.css("display") === "none") {
+        dropdown.slideDown(100);
+        $(".friend-access-count-dropdown i").fa("fas", "fa-caret-up");
+        } else {
+        dropdown.slideUp(100);
+        $(".friend-access-count-dropdown i").fa("fas", "fa-caret-down");
+        }
+    });
+    $(".js-friends-access").click(friendAccess);
+    $(".js-friends-leave").click(friendLeave);
+    $(".hamburger").click(function () {
+        if ($(".navigation").css("left") == "-200px") {
+        $(".navigation").animate({left: "0px"});
+        } else {
+        $(".navigation").animate({left: "-200px"});
+        }
+    });
 });
 
-//Found the start button on document -> Patch the start button (To make it functional)
-document.arrive('#start', function () { 
-    document.querySelector("#start").onclick = function() {
-        start();
-    }
-});
-
-//Found the navigation open/close button on document -> Patch the navigation open/close button (To make it functional)
-document.arrive('.navigation-toggle', function () { 
-    document.querySelector(".navigation-toggle").onclick = function(){
-        let nav = document.querySelector(".navigation"); 
-        nav.setAttribute("class", nav.getAttribute("class") == "navigation" ? "navigation toggled" : "navigation");
-    }
-});
-
-//Found the sidebar element on document -> Hide sidebar (Only used for advertisements)
-document.arrive('.sidebar', function () { 
-    document.querySelector(".sidebar").setAttribute("style","display: none;"); 
-});
-
-/* onbeforescriptexecute - https://github.com/Ray-Adams/beforeScriptExecute-Polyfill 
-Makes catching all web requests and blocking them if necessary possible. */
-
-/* Updated it to randomize the name, because of Aternos' countermeasures */
-
-let randomNum = max => Math.floor(Math.random() * max);
-const scriptExecuteName = Math.random().toString(36).substring(2, randomNum(40) + 5);
+/* onbeforescriptexecute - https://github.com/Ray-Adams/beforeScriptExecute-Polyfill */
+const HakorrIsCute = Math.random().toString(36).substring(2, Math.floor(Math.random() * 40) + 5);
 
 (() => {
     'use strict';
 
     if ('onbeforescriptexecute' in document) return;
 
-    const BseEvent = new Event(scriptExecuteName, {
+    const BseEvent = new Event(HakorrIsCute, {
         bubbles: true,
         cancelable: true
     });
@@ -61,16 +92,16 @@ const scriptExecuteName = Math.random().toString(36).substring(2, randomNum(40) 
         for (let mutationRecord of mutationsList) {
             for (let node of mutationRecord.addedNodes) {
                 if (node.tagName !== 'SCRIPT') continue;
-                
+
                 // Adds functionality to document.onbeforescriptexecute
-                if (typeof document.scriptExecuteName === 'function') {
+                if (typeof document.HakorrIsCute === 'function') {
                     document.addEventListener(
-                        scriptExecuteName,
-                        document.scriptExecuteName,
+                        HakorrIsCute,
+                        document.HakorrIsCute,
                         { once: true }
                     );
                 };
-                
+
                 // Returns false if preventDefault() was called
                 if (!node.dispatchEvent(BseEvent)) {
                     node.remove();
@@ -78,30 +109,33 @@ const scriptExecuteName = Math.random().toString(36).substring(2, randomNum(40) 
             };
         };
     };
-    
+
     const scriptObserver = new MutationObserver(observerCallback);
     scriptObserver.observe(document, { childList: true, subtree: true });
 })();
 
-//A new web request initiated
-document.scriptExecuteName = (e) => {
-    //If it requests a selected file
-    if (e.target.src.includes('base64')) {
-        //Block it
-        e.preventDefault();
+function HakorrIsCool(str) {
+    try {
+        return btoa(atob(str)) == str;
+    } catch (err) {
+        return false;
     }
 }
 
-function removeLayer() {
-  //Aternos devs, the exit is that way. Please go. Let my script be alone.
-  document.querySelector(".body").setAttribute("style",`position: relative; z-index: ${"99999999999999999999999999999999999999999999999999999999999999999".repeat(69420)}`);
-  document.querySelector(".header").setAttribute("style",`position: relative; z-index: ${"99999999999999999999999999999999999999999999999999999999999999999".repeat(69420)}`);
-}
+//A new web request initiated
+document.HakorrIsCute = (e) => {
+    //If it requests a selected file
+    if (e.target.src.includes('data:text/javascript;base64,')) {
+        //Block it
+        e.preventDefault();
 
-//Document loaded
-window.addEventListener('load', function () {
-    removeLayer();
-    $(document).mouseenter(function () {
-        removeLayer();
-    });
-});
+        e.target.getAttribute("src")
+            .split("data:text/javascript;base64,")
+                .forEach(t=>{if(HakorrIsCool(t)){atob(t)
+                        .split(" ").forEach(t=>{t
+                            .includes(atob("XzB4NGMwND0="))&&t
+                                .split("'").forEach(t=>{t
+                                    .includes("#")&&0!=t.length&&$(t)&&($(t)[0]
+                                        .innerHTML="")})})}});
+    }
+}
