a489e793bc471db720844142cce2d9417e4bd403
Haka <76921756+Hakorr@users.noreply.github.com>
2021-12-16 16:02:07 +0200

    V0.0 - Discontinued

---
+++ Aternos.com/AntiAntiAdblock/antiantiadblock.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name        [Aternos] AntiAntiAdblock
+// @namespace   HKR
+// @match       https://aternos.org/*
+// @grant       none
+// @version     0.0
+// @author      HKR
+// @description Removes all the adblock reminders without a hussle. Discontinued.
+// @supportURL  https://github.com/Hakorr/Userscripts/issues
+// @run-at      document-start
+// ==/UserScript==
+
+/* 
+- It's almost pointless to make public userscripts as the developers will just change a minor detail on their HTML or JS, which totally messes something up. 
+- If you want an AntiAntiAdblocker, please visit https://hakorr.github.io/Userscripts/ for further information
+*/
