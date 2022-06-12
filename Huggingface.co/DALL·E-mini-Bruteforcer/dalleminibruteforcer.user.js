aa0a33b0c62c3540affe83340c4f556cf2a9b95b
Haka <76921756+Hakorr@users.noreply.github.com>
2022-06-13 01:05:02 +0300

    Update dalleminibruteforcer.user.js

--- Huggingface.co/DALL·E-mini-Bruteforcer/dalleminibruteforcer.user.js
+++ Huggingface.co/DALL·E-mini-Bruteforcer/dalleminibruteforcer.user.js
@@ -9,8 +9,8 @@
 // @description Clicks the run button until success
 // ==/UserScript==
 
-if(window.location.hostname == "hf.space") {
-    (() => {
+(() => {
+    if(window.location.hostname == "hf.space") {
         const intervalMs = 5000;
 
         window.alert = txt => {
@@ -18,9 +18,9 @@ if(window.location.hostname == "hf.space") {
 
             if(txt.includes("Too much traffic")) {
                 console.warn(`\n\nWaiting ${intervalMs/1000} seconds and trying again...\n\n`);
-              
+
                 setTimeout(() => document.querySelector(".self-start").click(), intervalMs);
             }
         }
-    })();
-}
+    }
+})();
