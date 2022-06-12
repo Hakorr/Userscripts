c6ca8784cfa656f2e83a4688950a0eba7d70b4f7
Haka <76921756+Hakorr@users.noreply.github.com>
2022-06-13 00:35:33 +0300

    Create dalleminibruteforcer.user.js

---
+++ Huggingface.co/DALL·E-mini-Bruteforcer/dalleminibruteforcer.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name        DALL·E mini Bruteforcer
+// @namespace   HKR
+// @match       https://hf.space/static/dalle-mini/dalle-mini/index.html
+// @match       https://huggingface.co/spaces/dalle-mini/dalle-mini
+// @grant       none
+// @version     1.0
+// @author      HKR
+// @description Attempt to run until success
+// ==/UserScript==
+
+if(window.location.hostname == "hf.space") {
+    (() => {
+        const intervalMs = 5000;
+
+        window.alert = txt => {
+            console.log(txt);
+
+            if(txt.includes("Too much traffic")) {
+                console.warn(`\n\nWaiting ${intervalMs/1000} seconds and trying again...\n\n`);
+              
+                setTimeout(() => document.querySelector(".self-start").click(), intervalMs);
+            }
+        }
+    })();
+}
