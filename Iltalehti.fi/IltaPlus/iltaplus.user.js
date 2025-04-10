87eab694f15ffb2e8cd3ae497a64409d9392174c
Haka <76921756+Hakorr@users.noreply.github.com>
2025-04-10 21:51:38 +0300

    Update iltaplus.user.js

--- Iltalehti.fi/IltaPlus/iltaplus.user.js
+++ Iltalehti.fi/IltaPlus/iltaplus.user.js
@@ -3,7 +3,7 @@
 // @namespace   HKR
 // @match       https://www.iltalehti.fi/*
 // @grant       none
-// @version     2.1
+// @version     2.2
 // @author      HKR
 // @description Iltalehden "Plus-Tilaus" ilmaiseksi
 // @run-at      document-load
@@ -25,6 +25,8 @@ class ElementGenerator {
                 return this.relatedArticle(sectionData.article);
             case "image":
                 return this.image(sectionData);
+            case "directImage":
+                return this.directImage(sectionData);
             case "embed":
                 return this.embed.unknown(sectionData);
             case "divider":
@@ -37,7 +39,7 @@ class ElementGenerator {
 
         contentArr.forEach(item => {
             const type = item.type;
-            
+
             switch(type) {
                 case "text":
                     generatedHTML += `${item.text}`;
@@ -45,11 +47,11 @@ class ElementGenerator {
 
                 case "bold":
                     let boldText = "";
-    
+
                     item.items.forEach(boldItem => {
                         boldText += boldItem.text;
                     });
-    
+
                     generatedHTML += `<strong>${boldText}</strong>`;
                     break;
 
@@ -58,7 +60,7 @@ class ElementGenerator {
 
                     generatedHTML += `<a href="${item.url}">${generatedContent}</a>`;
                     break;
-                
+
                 case "italic":
                     let italicText = "";
 
@@ -68,7 +70,7 @@ class ElementGenerator {
 
                     generatedHTML += `<i>${italicText}</i>`;
                     break;
-                
+
                 default:
                     let text= "";
 
@@ -80,18 +82,18 @@ class ElementGenerator {
                     break;
             }
         });
-    
+
         return generatedHTML;
     }
 
     paragraph(contentArr) {
         let paragraph = document.createElement('p');
         paragraph.className = "paragraph";
-    
+
         let generatedHTML = this.content(contentArr);
-    
+
         paragraph.innerHTML = generatedHTML;
-    
+
         return paragraph;
     }
 
@@ -99,7 +101,7 @@ class ElementGenerator {
         const subheadline = document.createElement('h3');
         subheadline.className = "subheadline";
         subheadline.innerText = textContent;
-    
+
         return subheadline;
     }
 
@@ -125,44 +127,70 @@ class ElementGenerator {
             <span class="media-source">${imageData.properties.source}</span>
         </div>
         `;
-    
+
+        return image;
+    }
+
+    directImage(imageData) {
+        const image = document.createElement('div');
+        image.className = `article-image gallery ${imageData.float}`;
+        image.setAttribute("itemprop", "image");
+        image.setAttribute("itemscope", "");
+        image.setAttribute("itemtype", "http://schema.org/ImageObject");
+        image.setAttribute("role", "button");
+        image.setAttribute("tabindex", "0");
+        image.innerHTML = `
+        <div class="article-image-container" style="padding-bottom:${(imageData.height / imageData.width) * 100}%">
+            <img class="image image-show image-preview" src="${imageData.urls.size30}" alt="${imageData.caption}">
+            <img class="image image-show" src="${imageData.urls.size612}" srcset="${imageData.urls.size310} 310w, ${imageData.urls.size510} 510w, ${imageData.urls.size612} 612w" alt="${imageData.caption}">
+        </div>
+        <meta itemprop="url" content="${imageData.urls.size612}">
+        <meta itemprop="width" content="${imageData.width}">
+        <meta itemprop="height" content="${imageData.height}">
+        <div class="media-caption">
+            <meta itemprop="description" content="${imageData.caption}"/>
+            <span class="caption-text" itemprop="description">${imageData.caption}</span>
+            <span class="media-source">${imageData.source}</span>
+        </div>
+        `;
+
         return image;
     }
 
     list(items) {
         const listElem = document.createElement('div');
         listElem.className = "article-bullets";
-    
+
         const list = document.createElement('ul');
-    
+
         items.forEach(item => {
             let generatedHTML = this.content(item);
-    
+
             list.innerHTML += `<li>${generatedHTML}</li>`;
         });
-    
+
         listElem.appendChild(list);
-    
+
         return listElem;
     }
 
     aside(items) {
         const asideContainer = document.createElement('div');
         asideContainer.className = "aside-container";
-    
+
         const aside = document.createElement('div');
         aside.className = "aside";
-    
+
         items.forEach(item => {
             const type = item.type;
-    
+
             const sectionElement = this.handleType(type, item);
-    
+
             if(typeof sectionElement == "object") {
                 aside.appendChild(sectionElement);
             }
         });
-    
+
         asideContainer.appendChild(aside);
 
         return asideContainer;
@@ -171,10 +199,10 @@ class ElementGenerator {
     relatedArticle(article) {
         const relatedArticle = document.createElement('div');
         relatedArticle.className = "related-articles related-articles-within-text";
-    
+
         relatedArticle.innerHTML += `<h3>Lue myös</h3>`;
         relatedArticle.innerHTML += `<a href="/${article.category.category_name}/a/${article.article_id}">${article.headline}</a>`;
-    
+
         return relatedArticle;
     }
 
@@ -182,7 +210,7 @@ class ElementGenerator {
         const divider = document.createElement('div');
         divider.className = "article-divider";
         divider.innerHTML = `<div class="article-divider-content"></div>`;
-    
+
         return divider;
     }
 
@@ -191,13 +219,13 @@ class ElementGenerator {
             const twitterEmbed = document.createElement('div');
             twitterEmbed.className = "twitter-container article-embed";
             twitterEmbed.innerHTML = twitterData.embed_html;
-        
+
             return twitterEmbed;
         },
         default: embedData => {
             const embed = document.createElement('div');
             embed.innerHTML = embedData.embed_html;
-        
+
             return embed.firstChild;
         },
         unknown: embedData => {
@@ -225,7 +253,7 @@ const bypassPaywall = async () => {
         </a>
     </div>
     `;
-    
+
     const articleData = await fetch(`https://api.il.fi/v1/articles/${pageID}?include_main_media=true`)
         .then(response => response.json())
         .then(json => json.response);
@@ -233,6 +261,7 @@ const bypassPaywall = async () => {
     console.log("Article data: ", articleData);
 
     const articleBody = articleData.body;
+    const articleImages = articleData.ímages;
 
     const ElemGen = new ElementGenerator();
 
@@ -246,35 +275,42 @@ const bypassPaywall = async () => {
         }
     });
 
+    articleData?.["images"].forEach(sectionData => {
+        const sectionElement = ElemGen.handleType("directImage", sectionData);
+
+        if(typeof sectionElement == "object") {
+            articleBodyElement.appendChild(sectionElement);
+        }
+    });
+
     GM_addStyle(`
     .subheadline {
-         font-size: 18px; margin: 0 14px 15px; 
+         font-size: 18px; margin: 0 14px 15px;
     }
     .aside-container {
         clear: both;
         font-size: 16px;
         margin: 0 14px 14px;
         border: 1px solid #ddd;
-        background-color: #f7f7f7;
     }
     .aside-container h3:first-of-type {
-        color: #dc0000;
         margin: 14px;
     }
     .aside-container h3 {
-        color: #222;
         font-family: Bernino Sans Condensed,Arial,Verdana;
         font-size: 16px;
         font-weight: 700;
         margin: 10px 14px;
     }
     .paragraph {
-        color: #222;
         margin: 0 14px 15px;
         line-height: 1.45;
     }
+    .media-source {
+      opacity: 0.5;
+    }
     `);
-            
+
     console.log("Processed (n' bypassed) article:", articleBodyElement);
 
     document.querySelector(".article-body").innerHTML = articleBodyElement.innerHTML;
@@ -284,9 +320,9 @@ const bypassPaywall = async () => {
 let lastID = "";
 
 setInterval(() => {
-    let currentID = digitalData.page.attributes?.content?.cid;
+    let currentID = digitalData.page.attributes.content.cid;
 
-    if(typeof currentID == "string" && lastID != currentID)
+    if(lastID != currentID)
     {
         lastID = currentID;
         bypassPaywall();
